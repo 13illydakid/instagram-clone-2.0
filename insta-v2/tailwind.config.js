c729160ac567015814f318d09cf716edf4588cc6
Billy Chiu <billychiu517@gmail.com>
2023-10-01 05:03:19 -0700

    finished side panel feature

--- insta-v2/tailwind.config.js
+++ insta-v2/tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin');
+
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -18,21 +20,72 @@ module.exports = {
     require("@tailwindcss/forms"),
     require("tailwind-scrollbar"),
     require("tailwind-scrollbar-hide"),
+    plugin(function ({ addComponents }) {
+      addComponents({
+        '.navBtn': {
+          display: 'none',
+          height: '1.5rem',
+          '@screen md': {
+            display: 'inline-flex',
+          },
+          cursor: 'pointer',
+          '&:hover': {
+            transform: 'scale(1.25)',
+          },
+          transitionProperty: 'all',
+          transitionDuration: '150ms',
+          transitionTimingFunction: 'ease-out',
+        },
+        '.btn': {
+          height: '1.75rem',
+          cursor: 'pointer',
+          '&:hover': {
+            transform: 'scale(1.25)',
+          },
+          transitionProperty: 'all',
+          transitionDuration: '150ms',
+          transitionTimingFunction: 'ease-out',
+        },
+      })
+    }),
   ],
 };
 
+
+// /** @type {import('tailwindcss').Config} */
+// const plugin = require('tailwindcss/plugin');
 
 // module.exports = {
 //   mode: 'jit',
 //   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
 //   media: false,
-//   them: {
+//   theme: {
 //     extend: {},
 //   },
 //   variants: {
 //     extend: {},
 //   },
 //   plugins: [
-//     require("@tailwindcss/forms")
+//   require("@tailwindcss/forms"),
+//   require("tailwind-scrollbar"),
+//   require("tailwind-scrollbar-hide"),
+//     plugin(function ({ addComponents }) {
+//       addComponents({
+//         '.navBtn': {
+//           display: 'none',
+//           height: '1.5rem',
+//           '@screen md': {
+//             display: 'inline-flex',
+//           },
+//           cursor: 'pointer',
+//           '&:hover': {
+//             transform: 'scale(1.25)',
+//           },
+//           transitionProperty: 'all',
+//           transitionDuration: '150ms',
+//           transitionTimingFunction: 'ease-out',
+//         },
+//       })
+//     }),
 //   ],
 // };
