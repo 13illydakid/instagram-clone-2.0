5ffef299d54aa43438a4e80990a063ef3456a9a2
Billy Chiu <billychiu517@gmail.com>
2023-10-05 20:32:45 -0700

    s

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -7,7 +7,6 @@ export const authOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
     // ...add more providers here
   ],
