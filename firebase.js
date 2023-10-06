652dcae37526c2b282cb90b9f368a4e71295a5e8
Billy Chiu <billychiu517@gmail.com>
2023-10-05 20:18:17 -0700

    s

--- firebase.js
+++ firebase.js
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -20,8 +19,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
-const auth = getAuth(app);
 const db = getFirestore();
 const storage = getStorage();
 
-export { app, auth, db, storage };
+export { app, db, storage };
