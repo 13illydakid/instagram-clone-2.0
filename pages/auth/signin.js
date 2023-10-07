61a71a1a276797594112e42ffbed3e55e10646d5
Billy Chiu <billychiu517@gmail.com>
2023-10-07 01:03:33 -0700

    signin

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -20,7 +20,7 @@ function signIn({ providers }) {
                      <button
                         className="p-3 bg-blue-500 rounded-lg text-white"
                         onClick={() =>
-                           SignIntoProvider("google", { callbackUrl: "/" })
+                           SignIntoProvider(provider.id, { callbackUrl: "/" })
                         }
                      >
                         Sign in with {provider.name}
