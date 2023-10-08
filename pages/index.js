05adae0c8abacfe39285be6618b44658f886b7d9
Billy Chiu <billychiu517@gmail.com>
2023-10-07 20:15:54 -0700

    favicon

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <div className="bg-gray-50 h-screen overflow-y-scroll scrollbar-hide ">
       <Head>
-        <title>Presenting INSTAGRAM 2.0 BUILD</title>
+        <title>Billy's Projects</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
